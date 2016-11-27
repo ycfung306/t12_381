a97d238d4de25542be57f0684c65492eb292d148
Developer <comps381f@ouhk.edu.hk>
2016-11-27 22:45:53 +0800

    task 2 solution

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+app.get("/rectangle/:l/:w", function(req,res) {
+  var l = parseFloat(req.params.l);
+  var w = parseFloat(req.params.w);
+  var rectangle = {
+    "length"	: l,
+    "width"	: w,
+    "perimeter"	: (l + w) * 2,
+    "area"	: l * w
+  };
+  res.end(JSON.stringify(rectangle));
+});
+
+app.get("/circle/:r", function(req, res) {
+  var r = parseFloat(req.params.r);
+  var circle = {
+    "radius"	: r,
+    "perimeter"	: 2 * Math.PI * r,
+    "area"	: Math.PI * r * r
+  };
+  res.end(JSON.stringify(circle));
+});
+
+app.listen(process.env.PORT || 8099);
